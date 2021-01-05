341ff3a868cc5b22784a343cc1aa3cd119c9ff22
xBlackFish <waksmundzki.jakub@gmail.com>
2021-01-05 13:08:15 +0100

    binary gap fixed

--- binaryGap.js
+++ binaryGap.js
@@ -4,12 +4,25 @@ function dec2bin(dec){
 
 function binaryGap(n) {
     let binaryN = dec2bin(n);
-    let chars = binaryN.split('');
+    let arr = binaryN.split('');
+    let gapLength = 0;
 
+
+    if(n < 0 || n > 2147483647 || !arr.includes("0")) {
+        return 0;
+    }
+
+
+    while(arr.includes("1")) {
+        let left = arr.indexOf("1");
+        arr = arr.slice(left + 1);
+        let right = arr.indexOf("1");
+        arr = arr.slice(right);
+        if(gapLength < right - left) {
+            gapLength = right - left;
+        }
+    } 
+
+    return gapLength;
 
 }
-
-[1,2,3,1,1,14,4].reduce((acc, curr) => {
-    acc[curr] ? acc[curr]++ : acc[curr] = 1;
-    return acc;
-}, {})
